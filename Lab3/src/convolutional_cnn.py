989f3484f8c5bb60eba320d3403d44f75ef8aa28
Bespalov Max <bespalov.maxim.a@gmail.com>
2017-12-13 01:26:39 +0300

    Lab 3 basic convolutional cnn

--- Lab3/src/convolutional_cnn.py
+++ Lab3/src/convolutional_cnn.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.append('../../data/')
 import tfrecords_reader as reader
-from tfrecords_converter import rgb_to_y
 import tensorflow as tf
 import numpy as np
 
@@ -10,7 +9,9 @@ test_batch_size = 30
 batches_count = 1000
 tests_count = 100
 
-features_size = 320 * 320
+width = 320
+height = 320
+channels = 3
 hidden_layer1_size = 1000
 hidden_layer2_size = 300
 classes_size = 3
@@ -22,25 +23,36 @@ train_images, train_labels = reader.read (filename_train_queue)
 x_tensor, y_tensor = reader.next_batch3d(train_images, train_labels, batch_size)
 
 test_images, test_labels = reader.read (filename_test_queue)
-x_test_Tensor, y_test_Tensor = reader.next_batch3d(test_images, test_labels, test_batch_size)
+x_test_Tensor, y_test_Tensor = reader.next_batch3d (test_images, test_labels, test_batch_size)
 
-                                #CountOfImages, height, weight, RGB
-x = tf.placeholder(tf.float32, [None, features_size])
-
+x = tf.placeholder(tf.float32, [None, width, height, channels])
 y = tf.placeholder(tf.float32, [None, classes_size])
+W1 = tf.get_variable('W1', [5, 5, 3, 80], initializer=tf.random_normal_initializer())
 
-W1 = tf.get_variable('W1', [features_size, hidden_layer1_size], initializer=tf.random_normal_initializer())
-b1 = tf.get_variable('b1', [1,], initializer=tf.constant_initializer(0.0))
-y1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+conv1 = tf.nn.conv2d(x, W1, strides = [1, 1, 1, 1], padding="SAME")
+biases1 = tf.get_variable('biases1', [80], initializer=tf.constant_initializer(0.0))
+pre_activation1 = tf.nn.bias_add(conv1, biases1)
+conv1 = tf.nn.relu(pre_activation1)
 
-W2 = tf.get_variable('W2', [hidden_layer1_size, hidden_layer2_size], initializer=tf.random_normal_initializer())
-b2 = tf.get_variable('b2', [1,], initializer=tf.constant_initializer(0.0))
-y2 = tf.nn.relu(tf.matmul(y1, W2) + b2)
+pool1 = tf.nn.max_pool(conv1, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
-W3 = tf.get_variable('W3', [hidden_layer2_size, classes_size], initializer=tf.random_normal_initializer())
-b3 = tf.get_variable('b3', [1,], initializer=tf.constant_initializer(0.0))
-y_ = tf.nn.softmax(tf.matmul(y2, W3) + b3)
+W2 = tf.get_variable('W2', [5, 5, 80, 80], initializer=tf.random_normal_initializer())
+conv2 = tf.nn.conv2d(pool1, W2, strides = [1, 1, 1, 1], padding="SAME")
+biases2 = tf.get_variable('biases2', [80], initializer=tf.constant_initializer(0.0))
+pre_activation2 = tf.nn.bias_add(conv2, biases2)
+conv2 = tf.nn.relu(pre_activation2)
 
+pool2 = tf.nn.max_pool(conv2, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
+
+reshaped_size = 20 * 20 * 80
+shape = tf.stack([-1, reshaped_size])
+output = tf.reshape(pool2, shape)
+
+# output = tf.reshape(pool2, shape=[reshaped_size, -1, -1])
+W3 = tf.get_variable('W3', [reshaped_size, classes_size], initializer=tf.random_normal_initializer())
+biases3 = tf.get_variable('biases3', [classes_size], initializer=tf.constant_initializer(0.0))
+
+y_ = tf.nn.softmax(tf.matmul(output, W3) + biases3)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_),
                                 reduction_indices=[1]))
 
@@ -57,9 +69,8 @@ with tf.Session() as sess:
     for _ in range(batches_count):
         print (100 * _/batches_count,'%\r', end ='')
 
-        x_2d3c, y_b = sess.run ([x_tensor, y_tensor])
-        x_2d = rgb_to_y(x_2d3c[:,:,:,0], x_2d3c[:,:,:,1], x_2d3c[:,:,:,2])
-        x_b = x_2d.reshape(batch_size, -1) / 255
+        x_b, y_b = sess.run ([x_tensor, y_tensor])
+        x_b = x_b / 255
         vec_y = np.zeros([batch_size, 3])
         vec_y[range(batch_size), y_b] = 1
 
@@ -72,13 +83,11 @@ with tf.Session() as sess:
     sum_accuracy = 0
     for _ in range(tests_count):
         x_test,  y_test = sess.run ([x_test_Tensor, y_test_Tensor])
-        x_2d = rgb_to_y(x_2d3c[:,:,:,0], x_2d3c[:,:,:,1], x_2d3c[:,:,:,2])
-        x_test = x_2d.reshape(batch_size, -1) / 255  
+        x_test = x_test / 255.  
         resize_test_y = np.zeros ([len(y_test), 3])
         resize_test_y[range(len(y_test)), y_test] = 1   
         sum_accuracy += sess.run(accuracy, feed_dict={x: x_test, y: resize_test_y})
-    print("Accuracy: {:f}".format(sum_accuracy / tests_count))
-    print(sum_accuracy / tests_count)
+    print("Accuracy: {:f}".format(sum_accuracy / tests_count)) 
     
     coord.request_stop()
     coord.join(threads)
